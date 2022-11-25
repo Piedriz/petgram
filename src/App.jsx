1ff313ab9e2a08c877753b08d7e3f4aec40c8a9e
Santiago Piedriz <piedrizpasos@gmail.com>
2022-11-25 11:42:17 -0500

    :sparkles: Añadiendo logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 function App () {
   return (
     <>
       <GlobalStyles />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>
