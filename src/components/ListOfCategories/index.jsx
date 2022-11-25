b2da21e92cdf0b78dc09aac9d5ec0bfd22e6694b
Santiago Piedriz <piedrizpasos@gmail.com>
2022-11-25 15:18:36 -0500

    :alien: Consumiendo Api para renderizar categorias

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    getCategories()
+  }, [])
+  const getCategories = async () => {
+    const response = await axios('https://petgram-server-edsf8xpy2.now.sh/categories')
+    const data = await response.data
+    setCategories(data)
+  }
   return (
     <List>
       {categories.map((category) => (
--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -4,8 +4,8 @@ import { PhotoCard } from '../PhotoCard'
 export const ListOfPhotoCards = () => {
   return (
     <ul>
-      {[1, 2, 3, 4].map(id => (
-        <li><PhotoCard key={id} /></li>
+      {[1, 2, 3, 4].map((id, _) => (
+        <li><PhotoCard key={_} /></li>
       ))}
     </ul>
   )
