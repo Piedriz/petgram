3fa175ca8d51eeb543b524b4996471b58d9d159a
Santiago Piedriz <piedrizpasos@gmail.com>
2022-11-25 11:30:57 -0500

    :sparkles: añadiendo lista de photocards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 function App () {
   return (
     <>
       <GlobalStyles />
       <ListOfCategories />
+      <ListOfPhotoCards />
     </>
   )
 }
