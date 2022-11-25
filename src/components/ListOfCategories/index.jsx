6f84aa4ca8cd0088da8c2c2fc9622619fb4ea9e9
Santiago Piedriz <piedrizpasos@gmail.com>
2022-11-25 18:30:04 -0500

    :lipstick: Añadiendo lazy load a imagenes

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -3,8 +3,10 @@ import axios from 'axios'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+const useCategoriesData = () => {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     getCategories()
   }, [])
@@ -12,12 +14,36 @@ export const ListOfCategories = () => {
     const response = await axios('https://petgram-server-edsf8xpy2.now.sh/categories')
     const data = await response.data
     setCategories(data)
+    setLoading(!loading)
   }
-  return (
-    <List>
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false)
+  const { categories } = useCategoriesData()
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {categories.map((category) => (
         <Item key={category.id}><Category {...category} /></Item>
       ))}
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MdFavoriteBorder } from 'react-icons/md'
-import { ImgWrapper, Img, Button } from './styles'
+import { ImgWrapper, Img, Button, Article } from './styles'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_hamsters.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, setShow] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show &&
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes
+          </Button>
+        </>}
+    </Article>
   )
 }
